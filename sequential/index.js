363ae7fc92ba83d2df32335fb68db4e11d2c5aba
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-12-04 14:54:25 +0100

    Gestione errori con try catch in funzione main

--- sequential/index.js
+++ sequential/index.js
@@ -4,13 +4,19 @@ const {taskOne, taskTwo} = require('./tasks');
 
 async function main() {
 
-    console.time('Misurazione tempo di esecuzione');
-    const value1 = await taskOne();
-    const value2 = await taskTwo();
-    console.timeEnd('Misurazione tempo di esecuzione');
+    try {
+        console.time('Misurazione tempo di esecuzione');
+        const value1 = await taskOne();
+        const value2 = await taskTwo();
+        console.timeEnd('Misurazione tempo di esecuzione');
 
-    console.log('Valore di ritorno di task one ' + value1);
-    console.log('Valore di ritorno di task two ' + value2);
+        console.log('Valore di ritorno di task one ' + value1);
+        console.log('Valore di ritorno di task two ' + value2);
+
+    } catch (error) {
+        console.log(error);                
+    }
+
 }
 
 main();
