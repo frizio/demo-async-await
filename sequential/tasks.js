0a42407ed668b6df4debcf827c995fec641c43de
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-12-04 14:24:34 +0100

    Spiegazione util.promisify

--- sequential/tasks.js
+++ sequential/tasks.js
@@ -1,7 +1,10 @@
 
-//Funzione di js che esegue il codice in esso contenuto dopo un certo tempo. Utilizza callback
-setTimeout(function()  {});
+// Funzione di js che esegue il codice in esso contenuto dopo un certo tempo. Utilizza callback
+// setTimeout(function()  {});
 
+const util = require('util');
+// Converte funzioni maneggiate con callback in funzioni maneggiate con promise o async-await
+util.promisify(setTimeout);
 
 module.exports = {
 
