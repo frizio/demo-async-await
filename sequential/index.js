2aa674dccd76a127747b4b06d96ca6ab26b78f52
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-12-04 14:31:29 +0100

    Definizione e utilizzo base come  taskOne e taskTwo

---
+++ sequential/index.js
@@ -0,0 +1,9 @@
+//
+
+const {taskOne, taskTwo} = require('./tasks');
+
+const value1 = taskOne();
+const value2 = taskTwo();
+
+console.log('Valore di ritorno di task one ' + value1);
+console.log('Valore di ritorno di task two ' + value2);
