50c4047833750ca1185890855628867959ade9c7
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-12-04 14:37:51 +0100

    Esecuzione main e misurazione tempo di esecuzione

--- sequential/index.js
+++ sequential/index.js
@@ -4,9 +4,13 @@ const {taskOne, taskTwo} = require('./tasks');
 
 async function main() {
 
+    console.time('Misurazione tempo di esecuzione');
     const value1 = await taskOne();
     const value2 = await taskTwo();
+    console.timeEnd('Misurazione tempo di esecuzione');
 
     console.log('Valore di ritorno di task one ' + value1);
     console.log('Valore di ritorno di task two ' + value2);
 }
+
+main();
