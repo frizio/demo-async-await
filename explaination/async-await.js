3f6dc56570624305eb5ecb4dd1bc9048770497c6
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-12-04 12:37:03 +0100

    async + await 2

--- explaination/async-await.js
+++ explaination/async-await.js
@@ -1,11 +1,9 @@
 // Altra soluzione alla callback hell: ASYNC-AWAIT
 
-function requestHandler(req, res) {
+async function requestHandler(req, res) {
     
     // await User.findById(req.userId);
     // let user = await User.findById(req.userId);
     const user = await User.findById(req.userId);
-
-    
 
 }
